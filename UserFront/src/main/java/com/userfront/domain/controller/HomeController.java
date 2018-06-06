4b3437fba12f8060dfa3731998c078d92e743a25
vivek <vivek@cometnet-10-21-16-155.utdallas.edu>
2018-06-05 21:58:20 -0500

    added spring security for user signup, dao layers for remaining object

--- UserFront/src/main/java/com/userfront/domain/controller/HomeController.java
+++ UserFront/src/main/java/com/userfront/domain/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.userfront.domain.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.userfront.dao.RoleDao;
 import com.userfront.domain.User;
+import com.userfront.domain.security.UserRole;
 import com.userfront.service.UserService;
 
 @Controller
@@ -15,6 +20,9 @@ public class HomeController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private RoleDao roleDao;
 	
 	@RequestMapping("/")
 	public String home(){
@@ -50,7 +58,9 @@ public class HomeController {
 
             return "signup";
         } else {
-            userService.save(user);
+        	Set<UserRole> userRoles = new HashSet<>();
+        	userRoles.add(new UserRole(user,roleDao.findByName("ROLE_USER")));
+            userService.createUser(user, userRoles);
 
             return "redirect:/";
         }
