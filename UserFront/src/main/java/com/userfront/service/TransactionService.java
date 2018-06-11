5f11d740b0263903c0974363fb844e923527299c
vivek <vivek@sais-macbook-pro.local>
2018-06-11 18:35:25 -0500

    Added Transfer between accounts & add,edit,delete recipients

--- UserFront/src/main/java/com/userfront/service/TransactionService.java
+++ UserFront/src/main/java/com/userfront/service/TransactionService.java
@@ -1,8 +1,12 @@
 package com.userfront.service;
 
+import java.security.Principal;
 import java.util.List;
 
+import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.PrimaryTransaction;
+import com.userfront.domain.Recipient;
+import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.SavingsTransaction;
 
 public interface TransactionService {
@@ -17,4 +21,17 @@ public interface TransactionService {
     void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction);
     
     void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction);
+
+	void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount,
+			SavingsAccount savingsAccount) throws Exception;
+
+	void saveRecipient(Recipient recipient);
+
+	void deleteRecipientByName(String recipientName);
+
+
+	List<Recipient> findRecipientList(Principal principal);
+
+	Recipient findRecipientByName(String recipientName);
+
 }
--- UserFront/src/main/java/com/userfront/service/UserServiceImpl/TransactionServiceImpl.java
+++ UserFront/src/main/java/com/userfront/service/UserServiceImpl/TransactionServiceImpl.java
@@ -1,13 +1,24 @@
 package com.userfront.service.UserServiceImpl;
 
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.userfront.dao.PrimaryAccountDao;
 import com.userfront.dao.PrimaryTransactionDao;
+import com.userfront.dao.RecipientDao;
+import com.userfront.dao.SavingsAccountDao;
 import com.userfront.dao.SavingsTransactionDao;
+import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.PrimaryTransaction;
+import com.userfront.domain.Recipient;
+import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.SavingsTransaction;
 import com.userfront.domain.User;
 import com.userfront.service.TransactionService;
@@ -24,6 +35,15 @@ public class TransactionServiceImpl  implements TransactionService{
 	
 	@Autowired
 	SavingsTransactionDao savingsTransactionDao;
+	
+	@Autowired
+	PrimaryAccountDao primaryAccountDao;
+	
+	@Autowired
+	SavingsAccountDao savingsAccountDao;
+	
+	@Autowired
+	RecipientDao recipientDao;
 	
 	public List<PrimaryTransaction> findPrimaryTransactionList(String username){
 		User user = userService.findByUsername(username);
@@ -47,16 +67,74 @@ public class TransactionServiceImpl  implements TransactionService{
 		savingsTransactionDao.save(savingsTransaction);
 	}
 
-	@Override
+
 	public void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction) {
 		primaryTransactionDao.save(primaryTransaction);
 		
 	}
 
-	@Override
 	public void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction) {
 		savingsTransactionDao.save(savingsTransaction);
 		
+	}
+
+	public void betweenAccountsTransfer(String transferFrom, String transferTo, String amount,
+			PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception {
+		if(transferFrom.equalsIgnoreCase("Primary") && transferTo.equalsIgnoreCase("Savings")){
+			primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+			savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().add(new BigDecimal(amount)));
+			primaryAccountDao.save(primaryAccount);
+			savingsAccountDao.save(savingsAccount);
+			
+			Date date = new Date();
+			PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Trasnfer from Primary to Savings", "Transfer", "Finished", Double.parseDouble(amount), primaryAccount.getAccountBalance(), primaryAccount);
+			primaryTransactionDao.save(primaryTransaction);
+			SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Trasnfered from Primary to Savings", "Transfer", "Finished", Double.parseDouble(amount), savingsAccount.getAccountBalance(), savingsAccount);
+			savingsTransactionDao.save(savingsTransaction);
+			
+		} else if(transferFrom.equalsIgnoreCase("Savings") && transferTo.equalsIgnoreCase("Primary")){
+			primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().add(new BigDecimal(amount)));
+			savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+			primaryAccountDao.save(primaryAccount);
+			savingsAccountDao.save(savingsAccount);
+			
+			Date date = new Date();
+			SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Trasnfer from Savings to Primary", "Transfer", "Finished", Double.parseDouble(amount), savingsAccount.getAccountBalance(), savingsAccount);
+			savingsTransactionDao.save(savingsTransaction);
+			PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Trasnfered from Savings to Primary", "Transfer", "Finished", Double.parseDouble(amount), primaryAccount.getAccountBalance(), primaryAccount);
+			primaryTransactionDao.save(primaryTransaction);
+			
+			
+		}else {
+			throw new Exception("Invalid Transfer");
+		}
+	}
+
+
+	public void saveRecipient(Recipient recipient) {
+		recipientDao.save(recipient);
+		
+	}
+
+	@Override
+	public void deleteRecipientByName(String recipientName) {
+		recipientDao.deleteByName(recipientName);
+		
+	}
+
+	@Override
+	public List<Recipient> findRecipientList(Principal principal) {
+		String username = principal.getName();
+		List<Recipient> recipientList = ((Collection<Recipient>) recipientDao.findAll()).stream()
+				.filter(recipient -> username.equals(recipient.getUser().getUsername()))
+				.collect(Collectors.toList());
+		
+		return recipientList;
+	}
+
+	@Override
+	public Recipient findRecipientByName(String recipientName) {
+		return recipientDao.findByName(recipientName);
 	}
 
 
