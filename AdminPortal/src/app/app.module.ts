d36e9692c3d3068f97f25dd3ac669ac777ef1b41
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-06-18 02:03:42 -0500

    added role based access control and transactions list for admin

--- AdminPortal/src/app/app.module.ts
+++ AdminPortal/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { LoginComponent } from './login/login.component';
 import { LoginService } from './login.service';
 import { UserAccountComponent } from './user-account/user-account.component';
 import { UserService } from './user.service';
+import { PrimaryTransactionComponent } from './primary-transaction/primary-transaction.component';
+import { SavingsTransactionComponent } from './savings-transaction/savings-transaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    UserAccountComponent
+    UserAccountComponent,
+    PrimaryTransactionComponent,
+    SavingsTransactionComponent
   ],
   imports: [
     BrowserModule,
