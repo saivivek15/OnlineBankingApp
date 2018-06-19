6a77a98fa86416fd35be3a7b37d7062af088beff
Sai Vivek <vivek@Sais-MacBook-Pro.local>
2018-06-18 19:53:20 -0500

    added admin portal appointment component and backend apis

--- AdminPortal/src/app/app.module.ts
+++ AdminPortal/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { UserAccountComponent } from './user-account/user-account.component';
 import { UserService } from './user.service';
 import { PrimaryTransactionComponent } from './primary-transaction/primary-transaction.component';
 import { SavingsTransactionComponent } from './savings-transaction/savings-transaction.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { AppointmentService } from './appointment.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { SavingsTransactionComponent } from './savings-transaction/savings-trans
     LoginComponent,
     UserAccountComponent,
     PrimaryTransactionComponent,
-    SavingsTransactionComponent
+    SavingsTransactionComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +31,7 @@ import { SavingsTransactionComponent } from './savings-transaction/savings-trans
     HttpModule,
     routing
   ],
-  providers: [LoginService, UserService],
+  providers: [LoginService, UserService, AppointmentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
